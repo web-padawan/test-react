ea692d6c0fa018bcf4022c6bef86b37a7a113107
web-padawan <iamkulykov@gmail.com>
2023-12-05 16:10:43 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { TextField } from '@hilla/react-components/TextField.js';
+// import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <TextField label="Text field"></TextField>
+    </>
+  )
+}
+
+export default App
